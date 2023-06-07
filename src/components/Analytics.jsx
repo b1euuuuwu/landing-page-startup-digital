20ec4f6e2b8d231b777bfc0bc511e2e63a9f1033
Almas Mirzandi <almasmirzandir@gmail.com>
2023-06-07 08:35:24 +0700

    edit analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -8,7 +8,7 @@ const Analytics = () => {
             <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/'></img>
             <div className='flex flex-col justify-center'>
                 <p className='text-[#00FFFF] font-bold'>DATA ANALYTICS DASHBOARD</p>
-                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Track your logistics with our real-time tracking</h1>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Handle your logistics with our real-time tracking</h1>
                 <p className='text-left'>
                 Unlock the power of seamless logistic solution with shipmate.
 Imagine you can track your logistic in real-time tracking with customizable support tailored to your business. With shipmate you can benefits from expert support all while optimizing costs and enhancing efficiency.
